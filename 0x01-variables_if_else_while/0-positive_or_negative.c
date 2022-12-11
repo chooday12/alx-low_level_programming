5348b8402e4640db8983727952df7ab105386d38
chooday12 <chikefischer@gmail.com>
2022-12-10 20:34:35 -0800

    My 0-positive_or_negative.c commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  *main - prints if the number is positive, negative or 0
  *Return: Always 0
  */
-int main(void);
+int main(void)
 {
 	int n;
 
