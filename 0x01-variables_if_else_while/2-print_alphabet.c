a85d8d917b6d535566e820933ef73ba32b37d00b
chooday12 <chikefischer@gmail.com>
2022-12-13 00:45:08 -0800

    My 2-print_alphabet.c commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints alpha in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
