5fad2ffd885ea205116131bdef9f30b7804cc2ad
chooday12 <chikefischer@gmail.com>
2022-12-13 20:50:13 -0800

    my 4-isalpha.c commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,10 +6,11 @@
  *
  * Return: 1 if character is a letter, 0 otherwise
  */
+
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
-			return (1);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
 	else
-			return (0);
+		return (0);
 }
