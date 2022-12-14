6e7a422d7b10ccd12b39d0aed8aafa67ec3436d0
chooday12 <chikefischer@gmail.com>
2022-12-13 21:06:07 -0800

    my 5-sign.c commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: character to be checked
+ * Return: 1 if character is a letter, 0 otherwise
+ * 0 of number is zero
+ * -1 if number is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(',');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
