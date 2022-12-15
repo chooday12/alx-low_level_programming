20164dbae0c9ce60fea4f8d32fcd4e7383edfe30
chooday12 <chikefischer@gmail.com>
2022-12-15 10:54:16 -0800

    my 1-main.c commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *  * main - causes an infinite loop
+ *   * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("lnfinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 *while (i < 10)
+	 *{
+	 * putchar(i);
+	 *}
+	 */
+	printf("lnfinite loop avoided! \\o/\n");
+
+	return (0);
+}
