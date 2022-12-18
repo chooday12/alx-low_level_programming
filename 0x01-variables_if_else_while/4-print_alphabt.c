140960140b7ddec806c0e81f41247b7bbca4d0b9
chooday12 <chikefischer@gmail.com>
2022-12-18 06:44:47 -0800

    My 4-print_alphabt.c commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lower case
+ * main - prints the alphabet in lowercase
  * followed by a new line, except q and e
  * Return: Always 0
  */
