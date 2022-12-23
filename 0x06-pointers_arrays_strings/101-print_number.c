7eb9e056e16cf441e21d1bace5edd5c0f83a26fe
chooday12 <chikefischer@gmail.com>
2022-12-22 20:42:17 -0800

    my 101-print_number.c commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: The integer to be printed
+ * print_number - prints an integer.
+ * @n: The integer to be printed.
  */
 void print_number(int n)
 {
