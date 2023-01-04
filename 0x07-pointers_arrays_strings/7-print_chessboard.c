d18656db265da22839761cf7f7338ce8035ffc7b
chooday12 <chikefischer@gmail.com>
2023-01-04 10:33:10 -0800

    my 7-print_chessboard.c commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints the chessboard
- * @a: input pointers
- *
- * Return: no return
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Sucess)
  */
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, m = 0;
+	int i;
+	int j;
 
-	for (i = 0; i < 64; i++)
+	for (i = 0; i < 8; i++)
 	{
-		if (i % 8 == 0 && i != 0)
+		for (j = 0; j < 7; j++)
 		{
-			m = i;
-			_putchar('\n');
+			_putchar(a[i][j]);
 		}
-		_putchar(a[i / 8][i - m]);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
