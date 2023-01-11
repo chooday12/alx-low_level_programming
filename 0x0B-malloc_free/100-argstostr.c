26bdca34a25ef9b2d79e5129599c7f575cf9c09b
chooday12 <chikefischer@gmail.com>
2023-01-10 19:20:43 -0800

    my 100-argstostr.c commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -29,7 +29,7 @@ char *argstostr(int ac, char **av)
 		size++;
 		i++;
 	}
-	arg = malloc((sizeof(char) * size) + 1)
+	arg = malloc((sizeof(char) * size) + 1);
 	if (arg == NULL)
 		return (NULL);
 	i = 0;
