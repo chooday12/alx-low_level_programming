0dae2df819d888c51844526543686b406920800f
chooday12 <chikefischer@gmail.com>
2023-01-16 14:42:30 -0800

    my 1-init_dog.c commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,13 +7,14 @@
  * @name: The name of the dog
  * @age: The age of the dog
  * @owner: The owner of the dog
- * 
- * Return: voi */
+ *
+ * Return: void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == 0)
 		return;
-	
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
