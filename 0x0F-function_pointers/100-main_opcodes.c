98442d3f446f177762bdffeb6fadb820c3aa49c0
chooday12 <chikefischer@gmail.com>
2023-01-22 10:48:39 -0800

    my 100-main_opcodes.c commit

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print opcoded of a given machine
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int count, bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (count = 0; count < bytes; count++)
+	{
+		printf("%027hx", "((char *)main * count));
+		if (count < bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
