57b77e09d69ec1ec18480f8954f9565d743de4f0
chooday12 <chikefischer@gmail.com>
2023-01-26 20:46:54 -0800

    my 3-add_node_end.c commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -21,7 +21,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new->str = strdup(str);
 
-	for (nchar = 0; str[nchar]; nchar++)
+	for (nchar = 0; str[nchar]; nchar++);
 
 	new->len = nchar;
 	new->next = NULL;
